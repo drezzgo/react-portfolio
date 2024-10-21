bfc2e20523b2ffbe1b6c56ce41c94a21ffc438fc
Andres Gongora <agongoraramirez@gmail.com>
2024-10-21 10:04:53 -0500

    Fix the navbar logo and icon of the website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 const Navbar = ({ toggleLanguage, language }) => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="" />
+        <img className="mx-2 w-20" src={logo} alt="" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
         <a 
